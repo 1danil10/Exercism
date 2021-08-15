00e163e049372717e1adde23300f8bc66ee87266
1danil10 <danilmatvienko@gmail.com>
2021-08-15 11:02:03 +0300

    refactor: space age solution

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,25 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = (planet, seconds) => {
-  const orbitalPeriods = {
-    mercury: 0.2408467,
-    venus: 0.61519726,
-    earth: 1.0,
-    mars: 1.8808158,
-    jupiter: 11.862615,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132,
-  };
-  const EARTH_PERIOD_DAYS = 365.25;
+const orbitalPeriods = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
 
-  return +(
-    seconds /
-    60 /
-    60 /
-    24 /
-    EARTH_PERIOD_DAYS /
-    orbitalPeriods[planet]
-  ).toFixed(2);
+const EARTH_PERIOD_IN_SECONDS = 365.25 * 24 * 60 * 60;
+
+export const age = (planet, seconds) => {
+  return (
+    Math.round(
+      (seconds / EARTH_PERIOD_IN_SECONDS / orbitalPeriods[planet]) * 100,
+    ) / 100
+  );
 };
